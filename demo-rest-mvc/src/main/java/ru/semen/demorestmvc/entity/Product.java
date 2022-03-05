3239ab55f3894bbb325843afa9a35e7a1ff3a082
SemenSH <semenshaburov@gmail.com>
2022-03-05 22:45:50 +0500

    use lombok

--- demo-rest-mvc/src/main/java/ru/semen/demorestmvc/entity/Product.java
+++ demo-rest-mvc/src/main/java/ru/semen/demorestmvc/entity/Product.java
@@ -1,46 +1,17 @@
 package ru.semen.demorestmvc.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.Data;
 
-@Entity
 
+@Data
 public class Product {
     private Long id;
     private String title;
     private int price;
 
-    public Product() {
-    }
-
     public Product(Long id, String title, int price) {
         this.id = id;
         this.title = title;
         this.price = price;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Id
-    public Long getId() {
-        return id;
     }
 }
